33b05b12129d0878a7b7ced9fa288c89d175316c
sunalichandrasekaran <sunalichandrasekaran06@gmail.com>
2024-04-14 20:41:58 +0530

    adding product cards

--- src/pages/Home/views/Home.jsx
+++ src/pages/Home/views/Home.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import Slider from "../component/slider/Slider";
 import Banner from "../component/banner/Banner";
 import Feature from "../component/featurecard/Feature";
+import CardHeader from "../../Home/component/cardHeader/CardHeader";
+import ProductCard from "../component/ProductCard/ProductCard";
+import CategoryCard from "../../Home/component/categorycard/CategoryCard";
 const Home = () => {
-  return <div>
-    <Slider/>
-    <Banner/>
-    <Feature/>
-  </div>;
+  return (
+    <div>
+      <Slider />
+      <CardHeader title={"Today's"} subtitle={"Flash Sales"} timer={true} />
+      <div class="m-auto w-[85%] flex overflow-x-auto gap-[30px]  ">
+        <ProductCard />
+        <ProductCard />/
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+      <CardHeader
+        title={"Category"}
+        subtitle={"Browse by Category"}
+        timer={false}
+      />
+      <CategoryCard />
+      <Banner />
+      <CardHeader title={"Featured"} subtitle={"New Arrival"} timer={false} />
+      <Feature />
+    </div>
+  );
 };
 
 export default Home;
