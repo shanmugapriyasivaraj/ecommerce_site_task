67084fd3d4524db9bce38a5774e99caf9e1b3df3
sunalichandrasekaran <sunalichandrasekaran06@gmail.com>
2024-04-15 11:52:03 +0530

    changed size of the div

--- src/pages/Home/component/categorycard/Icons.jsx
+++ src/pages/Home/component/categorycard/Icons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Icons = (props) => {
   return (
     <>
-      <div class=" w-[170px] h-[145px]  hover:bg-[#DB4444] hover:text-[#fff] rounded border-2 border-black border-opacity-30 gap-[10px] flex flex-col justify-center items-center">
+      <div class="lg:w-[130px] lg:h-[130px] w-[170px] h-[145px]  hover:bg-[#DB4444] hover:text-[#fff] rounded border-2 border-black border-opacity-30 gap-[10px] flex flex-col justify-center items-center">
         <div class="text-[40px] ">{props.symbol}</div>
         <div class="text-[16px]  font-light">{props.symbolName}</div>
       </div>
