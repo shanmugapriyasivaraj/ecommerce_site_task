ee3e37ed29a78f8f1cdad4955184f2f4389d1a7d
kavipriyarupan <priya.k.sekarmsd@gmail.com>
2024-04-16 13:03:38 +0530

    created  features

--- src/pages/Home/views/Home.jsx
+++ src/pages/Home/views/Home.jsx
@@ -5,6 +5,11 @@ import Feature from "../component/featurecard/Feature";
 import CardHeader from "../../Home/component/cardHeader/CardHeader";
 import ProductCard from "../component/ProductCard/ProductCard";
 import CategoryCard from "../../Home/component/categorycard/CategoryCard";
+import Features from "../component/features/Features";
+import { FaTruckFast } from "react-icons/fa6";
+import { PiHeadsetDuotone } from "react-icons/pi";
+import { GoShieldCheck } from "react-icons/go";
+
 const Home = () => {
   return (
     <div>
@@ -12,7 +17,7 @@ const Home = () => {
       <CardHeader title={"Today"} subtitle={"Flash Sales"} timer={true} />
       <div class="m-auto w-[85%] flex overflow-x-auto gap-[30px]  ">
         <ProductCard />
-        <ProductCard />/
+        <ProductCard />
         <ProductCard />
         <ProductCard />
         <ProductCard />
@@ -26,6 +31,10 @@ const Home = () => {
       <Banner />
       <CardHeader title={"Featured"} subtitle={"New Arrival"} timer={false} />
       <Feature />
+      <div className="flex justify-between mx-[249px] mt-[100px] ">
+      <Features icon={<FaTruckFast />} title={'FREE AND FAST DELIVERY'} subtitle={'Free delivery for all orders over $140'}/>
+      <Features icon={<PiHeadsetDuotone />} title={'24/7 CUSTOMER SERVICE'} subtitle={'Friendly 24/7 customer support'}/>
+      <Features icon={<GoShieldCheck />} title={'MONEY BACK GUARANTEE'} subtitle={'We reurn money within 30 days'}/></div>
     </div>
   );
 };
